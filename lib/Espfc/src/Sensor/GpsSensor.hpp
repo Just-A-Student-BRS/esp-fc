b079b99ecb9f5e1d890a575ba70476008464b876
Ritesh DSB <1briteshs@gmail.com>
2026-07-30 21:36:09 +0530

    Fix include path and enhance GpsSensor functionality
    Corrected include path for SerialDevice and added State enum for GPS initialization. Fixed send method to include missing 'msg' parameter.

--- lib/Espfc/src/Sensor/GpsSensor.hpp
+++ lib/Espfc/src/Sensor/GpsSensor.hpp
@@ -1,18 +1,36 @@
 #pragma once
 
 #include "Sensor/BaseSensor.hpp"
-#include "../../Device/SerialDevice.hpp"
+#include "Device/SerialDevice.hpp" // Correct include path
 #include "Gps.hpp"
+#include "Utils/Timer.hpp"
 
 namespace Espfc::Sensor {
 
 class GpsSensor : public BaseSensor
 {
 public:
+  // Define the State enum required for the GPS initialization sequence
+  enum State {
+    RECEIVE,
+    WAIT,
+    DETECT_BAUD,
+    GET_VERSION,
+    CONFIGURE_BAUD,
+    DISABLE_NMEA,
+    ENABLE_UBX,
+    ENABLE_NAV5,
+    ENABLE_SBAS,
+    DETECT_GPS_L5,
+    CONFIGURE_GNSS,
+    CONFIGURE_NAV_RATE,
+    ERROR
+  };
+
   GpsSensor(Model& model);
 
-  int begin(Device::SerialDevice* port, int baud) override;
-  int update() override;
+  int begin(Device::SerialDevice* port, int baud);
+  int update();
 
   void calculateHomeVector() const;
 
@@ -44,26 +62,34 @@ private:
   void checkSupport(const char* payload) const;
 
   bool isLegacyProto() const { return _model.state.gps.support.version < GPS_M10; }
+  
   void send(const auto& msg, State nextState, State timeoutState)
   {
     _ubxMsg = Gps::UbxMessage();
     _port->write(msg.getData(), msg.getSize());
     setState(nextState, nextState, timeoutState);
   }
+  
   void send(const auto& msg, State nextState)
   {
-    send(nextState, nextState, ERROR);
+    // FIX: 'msg' was missing here in the previous broken version
+    send(msg, nextState, ERROR); 
   }
 
   Device::SerialDevice* _port;
   Gps::UbxParser _ubxParser;
   Gps::UbxMessage _ubxMsg;
   Gps::NmeaMessage _nmeaMsg;
+  Utils::Timer _timer;
+  
   uint32_t _timeout;
   int _targetBaud;
   int _currentBaud;
+  
+  State _state;
   State _ackState;
   State _timeoutState;
+  
   uint8_t _counter;
   uint8_t _fixHoldCounter;
 };
